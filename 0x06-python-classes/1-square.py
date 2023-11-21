fb90138667a8bdb470bcbc167c6f0f4c2e8dd919
dennismutitu006 <dennismutitu06@gmail.com>
2023-11-21 17:55:44 +0000

    square with a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square using the init m3thod."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
