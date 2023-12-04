f3e6453ad12d003c16d5663eac0fc8fa1cd2126c
dennismutitu006 <dennismutitu06@gmail.com>
2023-12-04 11:57:49 +0000

    exception

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -3,8 +3,8 @@
 
 
 class BaseGeometry:
-    """Represent base geometry."""
+	"""Represent base geometry."""
 
-    def area(self):
-        """Not implemented."""
-       	raise Exception("area() is not implemented")
+	def area(self):
+		"""Not implemented."""
+		raise Exception("area() is not implemented")
