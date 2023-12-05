539dfa5e6a01a9f7df40d4c9cfe08d10c47edfaa
dennismutitu006 <dennismutitu06@gmail.com>
2023-12-05 10:12:56 +0000

    check code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ definaation of a function that reads a text file"""
+
+
 def read_file(filename=""):
 	""" read file will allow input of the file name"""
-	with open("my_file_0.txt", "r") as f:
+	with open(filename, encoding="utf-8") as k:
 		""" do the actual readding"""
-		print(f.read())
+		print(f.read(), end="")
