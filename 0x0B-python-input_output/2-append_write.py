2349fa15ddf2b156975b61735b08d7084a127171
dennismutitu006 <dennismutitu06@gmail.com>
2023-12-05 10:50:09 +0000

    append with docstring

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Writes a file appending function"""
+
+
 def append_write(filename="", text=""):
+	"""appends a string to the end of a UTF* txt file"""
 	with open(filename, "a", encoding="utf-8") as f:
 		return f.write(text)
