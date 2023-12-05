aab4bb13ef14ce60b3702827b33e954feb6ac7f0
dennismutitu006 <dennismutitu06@gmail.com>
2023-12-05 10:14:33 +0000

    sanitizer

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
 	""" read file will allow input of the file name"""
 	with open(filename, encoding="utf-8") as k:
 		""" do the actual readding"""
-		print(f.read(), end="")
+		print(k.read(), end="")
