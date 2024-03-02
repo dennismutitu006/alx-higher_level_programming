87501b1887fdf8257dc6b4a3905378e02ce2eb1c
Dennis <dennismutitu06@gmail.com>
2024-03-02 13:42:44 -0800

    another one

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,11 +8,11 @@ import sys
 import urllib.request
 
 # Get the url from the 1st argument
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
 # After getting the url , send a req to url and open resonse.
 with urllib.request.urlopen(url) as response:
     # in the opened url get the x-requested-id var from the header
     x_req_id = response.getheader("X-Request-Id")
     print(x_req_id)
-if __name__ == "__main__":
